3f4a52073612213f78ba67557d4359092875d2cd
Kushagra Sikka <kushagrasikka@gmail.com>
2025-05-09 20:16:40 +0000

    Final improvements and cleanup

---
+++ client/src/components/loading.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { CircularProgress } from '@mui/material';
+
+const Loading = ({ disableShrink }) => (
+  <CircularProgress
+    disableShrink={disableShrink} // disable under heavy loads to avoid animation problems
+    style={{
+      position: 'fixed',
+      top: '50%',
+      bottom: '50%',
+      left: '50%',
+      right: '50%',
+      zIndex: 100,
+    }}
+  />
+);
+
+Loading.propTypes = {
+  disableShrink: PropTypes.bool,
+};
+
+Loading.defaultProps = {
+  disableShrink: false,
+};
+
+export default Loading;
+
+// TODO: Consider refactoring for better performance
